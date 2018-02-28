a1c34f0d7a4d852f250c07835b5db1350a029fef
Michael Sumner <mdsumner@gmail.com>
2018-02-28 16:22:01 +0000

    GOUGH data

---
+++ data-raw/GOUGH.R
@@ -0,0 +1,12 @@
+# I have attached processed data from a GLS tag deployed on a Grey
+# Petrel from Gough Island. The data belongs to Ben Dilley and Prof
+# Peter Ryan from the Percy Fitzpatrick Institute for Ornithology at
+# UCT. They have passed it on to me for further analysis but are happy
+# for it to be featured on SGAT. I have edited the code to account for
+# the nuances of Greys, but can standardise it if you need. I chose this
+# one as the equinox period isn't catastrophic. The equinox issue seems
+# a little worse for these guys than for the albies, shearwaters, ellies
+# and furries...perhaps the more northerly latitude? I also have a
+# couple with particularly bad light data if you need something of that
+# flavour...
+
